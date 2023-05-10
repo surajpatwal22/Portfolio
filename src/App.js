1142b9338a52e66238c3cc3767e858b6b5e8d219
surajpatwal22 <patwal0suraj@gmail.com>
2023-05-10 13:14:29 +0530

    portfoliosxn

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Navbar from './Components/Navbar';
 import  Home  from "./Components/Home";
 import Sociallinks from './Components/Sociallinks';
 import About from "./Components/About";
+import Portfolio  from "./Components/Portfolio";
+
 
 const App = () => {
  return(
@@ -11,6 +13,8 @@ const App = () => {
     <Navbar />
     <Home />
     <About/>
+    <Portfolio />
+    
 
     <Sociallinks />
     
