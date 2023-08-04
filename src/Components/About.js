f8c1a5e97ec040bf80bea8e878ed70d5069a43a8
surajpatwal22 <patwal0suraj@gmail.com>
2023-08-04 11:34:32 +0530

    final touch

--- src/Components/About.js
+++ src/Components/About.js
@@ -8,11 +8,20 @@ const About = () => {
                 <p className='text-4xl font-bold inline border-b-4 border-gray-500 '>About</p>
             </div>
 
-            <p className='text-xl mt-20'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum nam, nulla fugit, quidem nobis libero magni consectetur maxime iste laborum, reiciendis blanditiis animi labore esse corrupti! Labore doloribus reiciendis repudiandae!</p>
+            <p className='text-xl mt-20'>I am a passionate and versatile individual, holding a degree in Civil Engineering and driven by an insatiable curiosity for technology and web development. Eager to explore new horizons, I embarked on a transformative journey, dedicating nine months to completing a comprehensive full-stack web development course at Ineuron.</p>
 
             <br />
 
-            <p className='text-xl'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil, modi cumque! Dolorem sequi ducimus, tempora unde incidunt, doloremque ab, molestias cupiditate aut assumenda voluptate debitis ipsam aperiam ipsum quasi tenetur?  </p>
+            <p className='text-xl'>My goal is to make a meaningful impact in the digital landscape while leveraging my problem-solving skills to address real-world challenges with creativity and precision.
+            
+            My strengths lie in my unwavering commitment to hard work, collaborative spirit as a team player, and an open-minded approach to embracing new challenges and ideas. These attributes empower me to thrive in dynamic environments and drive innovation, leveraging my diverse skill set to create impactful solutions that merge technology and engineering seamlessly.  
+            I am excited to embark on a fulfilling career journey, leveraging my diverse skill set to create a brighter future in the realm of technology and engineering. 🚀🌐 
+
+
+
+
+
+ </p>
         </div>
     </div>
   )
--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -13,35 +13,41 @@ const Portfolio = () => {
       id: 1,
       scr: mealdb,
       href: "https://mealdbapp.netlify.app/",
+      name:"MEAL-APP"
     },
     {
       id: 2,
       scr: todo,
       href: "https://to-doapp371.netlify.app/",
+      name:"TODO-LIST APP"
 
     },
     {
       id: 3,
       scr: pokemonapp      ,
        href: "https://pokemondetailapp.netlify.app/",
+       name:"POKEMON-APP"
 
     },
     {
       id: 4,
       scr: movieapp ,
       href: "https://watchmoviesdetailnow.netlify.app/",
+      name:"SEARCH-MOVIE-APP"
 
     },
     {
       id: 5,
       scr: calculatorapp,
       href: "https://calculatorapp234.netlify.app/",
+      name:"CALCULATOR-APP"
 
     },
     {
       id: 6,
       scr:  rockpaperscissor,
       href: "https://playgamewithcomputer.netlify.app/",
+      name:"GAME-APP"
 
     }
   ]
@@ -56,14 +62,17 @@ const Portfolio = () => {
         <div className=' grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0'>
 
           {
-            portfolios.map(({ id, scr , href }) => (
+            portfolios.map(({ id, scr , href, name  }) => (
 
               <div key={id} className=' shadow-md shadow-gray-600 rounded-lg'>
             <img src={scr} alt="" srcset=""
               className='  rounded-md duration-200 hover:scale-105' />
-            <div className='flex items-center justify-center'>
-              <button className=' w-1/2 px-6 py-3 m-1 duration-200 hover:scale-105' > <a href={href}> Demo</a> </button>
-              {/* <button className=' w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Code</button> */}
+            <div className='flex items-center justify-center flex-col'>
+              <h1 className='p-2 text-2xl text-amber-500'>{name}</h1>
+              <button className=' w-1/2 px-6 py-3 m-1 duration-200 hover:scale-105' > 
+              <a href={href} 
+              target='_blank' rel='noreferrer' > Demo</a> </button>
+              {/* <button className=' w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>{name}</button> */}
             </div>
           </div>
           ))
